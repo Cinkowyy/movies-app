6f8515e0f908641272f638077888d4b4b6f03269
Cinkowyy <90154960+Cinkowyy@users.noreply.github.com>
2022-06-22 21:45:48 +0200

    build: create react native app

---
+++ App.js
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+          backgroundColor: "#F0F8FF"
+      }}
+    >
+      <Text>Hello React Native!</Text>
+    </View>
+  );
+}
